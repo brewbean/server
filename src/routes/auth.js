3af6eb18e2eda0a74e6db869cf11bdf6abda45f6
devgladstone <mistuhgladstone@gmail.com>
2021-02-11 14:53:35 -0800

    add helmet & rate limiter & cookie options

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,13 +7,13 @@ import {
   logoutAllController,
 } from "../controllers/auth.js";
 import PasswordRouter from "./password.js";
-const { AUTH_RATE_MINUTES, AUTH_RATE_LIMIT } = process.env
+const { AUTH_RATE_LIMITER_MINUTES, AUTH_RATE_LIMIT_MAX } = process.env;
 
 const router = express.Router();
 
 const createAccountLimiter = rateLimit({
-  windowMs: AUTH_RATE_MINUTES * 60 * 1000, // 1 hour window
-  max: AUTH_RATE_LIMIT, // start blocking after 5 requests
+  windowMs: AUTH_RATE_LIMITER_MINUTES * 60 * 1000, // 1 hour window
+  max: AUTH_RATE_LIMIT_MAX, // start blocking after 5 requests
   message: "Too many attempts from this IP, please try again after an hour",
 });
 
