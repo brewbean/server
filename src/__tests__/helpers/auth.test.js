75cc5538897dc85ac1dc687befbd0cab9ab89870
William Yang <williamyang93@gmail.com>
2021-02-24 19:34:40 -0800

    First successful mock validate credential test

--- src/__tests__/helpers/auth.test.js
+++ src/__tests__/helpers/auth.test.js
@@ -1,14 +1,40 @@
-import getDuplicateError, { validateCredentials } from '../../helpers/auth'
+import getDuplicateError, { validateCredentials } from "../../helpers/auth";
+import bcrypt from "bcrypt";
+import axios from "axios";
 
+jest.mock("axios");
 // test('Check if email already exists', () => {
 //   let error = "Something barista_email_key";
 //   console.log(getDuplicateError(error));
 //   expect(getDuplicateError(error)).toBe();
 // })
 
-test('Validate Credentials to Strict Equal', async () => {
-  let email = 'jest-test@test.com'
-
+test("Validate Credentials returns correct valid and barista values", async () => {
+  const email = "jest-test@test.com";
+  const password = "TestPassword123!";
+  const encryptedPassword = await bcrypt.hash(password, 10)
+  const valid = await bcrypt.compare(password, encryptedPassword);
+  const expected = {
+    valid: valid,
+    barista: {
+      id: 2,
+      email: "jest-test@test.com",
+      password: encryptedPassword,
+    },
+  };
+  axios.post.mockResolvedValue({
+    data: {
+      data: {
+        barista: [
+          {
+            id: 2,
+            email: "jest-test@test.com",
+            password: encryptedPassword
+          }
+        ]
+      }
+    },
+  });
   const data = await validateCredentials(email, password);
-  expect(data).toStrictEqual(expected)
-})
+  expect(data).toEqual(expected);
+});
